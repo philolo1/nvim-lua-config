528cce62e4ad9d7d04df78f2c7f9798482fd51b9
Patrick Klitzke <philologos14@gmail.com>
2023-09-05 20:12:41 +0900

    Update lsp.

--- lua/philolo1/lsp.lua
+++ lua/philolo1/lsp.lua
@@ -47,3 +47,25 @@ end)
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+
+
+-- Configure autocomplete
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  enabled = true,
+  completion = { completeopt= "menu,menuone"},
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    -- ['<C-n>'] = cmp_action.luasnip_jump_forward(),
+    -- ['<C-m>'] = cmp_action.luasnip_jump_backward(),
+  }
+})
