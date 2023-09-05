51db324a4185aa8494d359311923d10d30887060
Patrick Klitzke <philologos14@gmail.com>
2023-09-05 19:37:54 +0900

    Update lsp

--- lua/philolo1/lsp.lua
+++ lua/philolo1/lsp.lua
@@ -27,23 +27,7 @@ lsp.setup_servers({
 --   mapping = cmp_mappings
 -- })
 --
--- lsp.set_preferences({
---     suggest_lsp_servers = false,
---     sign_icons = {
---         error = 'E',
---         warn = 'W',
---         hint = 'H',
---         info = 'I'
---     }
--- })
 --
---
---
--- end)
---
---
---
-
 
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
@@ -54,15 +38,8 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>d", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
-
-  -- TODO
-  --   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  --   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  --   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  --   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  --   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  --   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  --   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
 end)
 
 
