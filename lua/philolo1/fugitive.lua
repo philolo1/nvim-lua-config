e8f9906808e6a94e2eade222a6a328e64b7704bb
Patrick Klitzke <philologos14@gmail.com>
2023-09-07 00:41:42 +0900

    Change fugitive shortcuts

--- lua/philolo1/fugitive.lua
+++ lua/philolo1/fugitive.lua
@@ -1,3 +1,1 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
-vim.keymap.set("n", "<leader>gp", ":Git push<CR>");
-vim.keymap.set("n", "<leader>gc", ":Git commit<CR>");
+vim.keymap.set("n", "<C-g>", vim.cmd.Git);
