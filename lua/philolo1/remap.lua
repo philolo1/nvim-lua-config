74e6f6cf7dfd94651cc14447962d8ff115e17c8d
Patrick Klitzke <philologos14@gmail.com>
2023-09-07 15:34:35 +0900

    update remap autocmd

--- lua/philolo1/remap.lua
+++ lua/philolo1/remap.lua
@@ -20,6 +20,16 @@ vim.keymap.set("n", "K", "{")
 vim.keymap.set("v", "J", "}")
 vim.keymap.set("v", "K", "{")
 
+
+
+
+-- My own user commands
+vim.api.nvim_create_user_command("V", ":e /Users/philolo1/.config/nvim/lua/philolo1/init.lua", {});
+vim.api.nvim_create_user_command("S", ":source", {});
+
+vim.api.nvim_create_user_command("H", ":echo 'hi'", {});
+
+
 function OpenDiagnostics()
     vim.cmd(":lua vim.diagnostic.setloclist()");
 end
@@ -33,8 +43,24 @@ vim.api.nvim_create_autocmd(
 );
 
 
--- My own user commands
-vim.api.nvim_create_user_command("V", ":e /Users/philolo1/.config/nvim/lua/philolo1/init.lua", {});
-vim.api.nvim_create_user_command("S", ":source", {});
+function MkNonExDir(file, buf)
+    if vim.fn.getbufvar(buf, 'buftype') == "" and not string.match(file, "^%w+:/") then
+        local dir = vim.fn.fnamemodify(file, ':h')
+        local is_dir = vim.fn.isdirectory(dir);
+        if is_dir == 0 then
+            vim.fn.mkdir(dir, 'p')
+        end
+    end
+end
 
-vim.api.nvim_create_user_command("H", ":echo 'hi'", {});
+-- Create an autocommand group using vim.api.nvim_command
+vim.api.nvim_create_autocmd(
+    { "BufWritePre" },
+    {
+        callback = function()
+            local file = vim.fn.expand("<afile>");
+            local buffer = vim.fn.expand("<abuf>");
+            MkNonExDir(file, buffer);
+        end
+    }
+)
