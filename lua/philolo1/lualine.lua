4377ce71d15064e95c1cec14ec75d4a2f76139f2
Patrick Klitzke <philologos14@gmail.com>
2023-09-07 14:20:16 +0900

    Lualine: use relative paths

--- lua/philolo1/lualine.lua
+++ lua/philolo1/lualine.lua
@@ -1,1 +1,12 @@
-require('lualine').setup()
+require('lualine').setup(
+    {
+        sections = {
+            lualine_a = {
+                {
+                    'filename',
+                    file_status = true, -- displays file status (readonly status, modified status)
+                    path = 1            -- 0 = just filename, 1 = relative path, 2 = absolute path
+                }
+            }
+        }
+    })
